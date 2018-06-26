81f2de18a7f91f6674dc1f8214703610d92a6602
annaliner <annaliner89@gmail.com>
2018-06-26 16:29:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,44 @@
-require_relative 'config/environment'
+# require "sinatra"
+
+# class App < Sinatra::Base
+  
+#   get '/' do 
+#     "i love bok choy."
+    
+# end
+
+#   get '/nutritious' do 
+#     "has lots of fiber"
+    
+#   end
+
+# end 
+
+require "sinatra"
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Anna"
+  end
+  
+  get '/hometown' do
+    "My hometown is Castro Valley"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Truly Julie's Blues by Cut Worms"
+  end
+  
 end
+
+
+
+
+
+
+
