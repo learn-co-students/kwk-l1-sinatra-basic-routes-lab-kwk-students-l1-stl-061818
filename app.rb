8d28566c4c85dcffae72490460e758642e2a544f
jli063 <jli063@rsdmo.org>
2018-06-26 16:27:02 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,32 @@
-require_relative 'config/environment'
+require 'sinatra'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Jessica"
+  end
+  
+  get '/hometown' do 
+    "My hometown is St.Louis"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Love Story"
+  end
+  
 end
+    
+  
+#   get '/' do 
+#     "I love bok choy"
+    
+# end
+
+#   get '/nutritious' do
+#     <h>"has lots of fiber"</h1>
+#   end
+
